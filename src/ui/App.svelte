7f1e53663e0d0737d1bb68c8a94a763b31a60c89
MattBrooks95 <28607360+MattBrooks95@users.noreply.github.com>
2022-05-01 13:07:10 +0900

    svelte-side type checking

--- src/ui/App.svelte
+++ src/ui/App.svelte
@@ -6,6 +6,9 @@ if (testCallback) {
 } else {
 	console.log('no callback in props', $$props);
 }
+//x();//no in-editor error messages, probably because I don't have a language server for svelte yet
+//BUT npx svelte-check will catch this
+
 </script>
 
 <main>
